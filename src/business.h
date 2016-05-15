ef9fb04f30f21d49eed7b7812807c6c1f3347d24
Ji Hoon Choi <snesiscool@gmail.com>
2016-05-14 19:36:38 -0700

    Better code organization

--- src/business.h
+++ src/business.h
@@ -24,132 +24,154 @@ class Business
         std::vector<Item*> items;
 
     public:
-        Business(std::string name, double cashOnHand, double assetsValue, double rating, int month)
-        {
-            this->name = name;
-            this->cashOnHand = cashOnHand;
-            this->assetsValue = assetsValue;
-            this->rating = rating;
-            this->month = month;
-            income = 0;
-            expenses = 0;
-            wages = 0;
-            execSalary = 0;
-            numStores = 0;
-            numBuildings = 0;
-            numEmployees = 0;
-        }
-
-        void setCashOnHand(double cashOnHand)
-        {
-            this->cashOnHand = cashOnHand;
-        }
-
-        void setAssetsValue(double assetsValue)
-        {
-            this->assetsValue = assetsValue;
-        }
-
-        void setRating(double rating)
-        {
-            this->rating = rating;
-        }
-
-        void setMonth(int month)
-        {
-            this->month = month;
-        }
-
-        void setIncome(double income)
-        {
-            this->income = income;
-        }
-
-        void setExpenses(double expenses)
-        {
-            this->expenses = expenses;
-        }
-
-        void setWages(double wages)
-        {
-            this->wages = wages;
-        }
-
-        void setExecSalary(double execSalary)
-        {
-            this->execSalary = execSalary;
-        }
-
-        void setStores(int numStores)
-        {
-            this->numStores = numStores;
-        }
-
-        void setBuildings(int numBuildings)
-        {
-            this->numBuildings = numBuildings;
-        }
-
-        void setEmployees(int numEmployees)
-        {
-            this->numEmployees = numEmployees;
-        }
-
-        std::string getName()
-        {
-            return name;
-        }
-
-        double getCashOnHand()
-        {
-            return cashOnHand;
-        }
-
-        double getAssetsValue()
-        {
-            return assetsValue;
-        }
-
-        double getRating()
-        {
-            return rating;
-        }
-
-        int getMonth()
-        {
-            return month;
-        }
-
-        double getIncome()
-        {
-            return income;
-        }
-
-        double getExpenses()
-        {
-            return expenses;
-        }
-
-        double getWages()
-        {
-            return wages;
-        }
-
-        double getExecSalary()
-        {
-            return execSalary;
-        }
-
-        int getStores()
-        {
-            return numStores;
-        }
-
-        int getBuildings()
-        {
-            return numBuildings;
-        }
-
+        Business(std::string name, double cashOnHand, double assetsValue, double rating, int month);
+        void setCashOnHand(double cashOnHand);
+        void setAssetsValue(double assetsValue);
+        void setRating(double rating);
+        void setMonth(int month);
+        void setIncome(double income);
+        void setExpenses(double expenses);
+        void setWages(double wages);
+        void setExecSalary(double execSalary);
+        void setStores(int numStores);
+        void setBuildings(int numBuildings);
+        void setEmployees(int numEmployees);
+        std::string getName();
+        double getCashOnHand();
+        double getAssetsValue();
+        double getRating();
+        int getMonth();
+        double getIncome();
+        double getExpenses();
+        double getWages();
+        double getExecSalary();
+        int getStores();
+        int getBuildings();
 };
 
+Business::Business(std::string name, double cashOnHand, double assetsValue, double rating, int month)
+{
+    this->name = name;
+    this->cashOnHand = cashOnHand;
+    this->assetsValue = assetsValue;
+    this->rating = rating;
+    this->month = month;
+    income = 0;
+    expenses = 0;
+    wages = 0;
+    execSalary = 0;
+    numStores = 0;
+    numBuildings = 0;
+    numEmployees = 0;
+}
+
+void Business::setCashOnHand(double cashOnHand)
+{
+    this->cashOnHand = cashOnHand;
+}
+
+void Business::setAssetsValue(double assetsValue)
+{
+    this->assetsValue = assetsValue;
+}
+
+void Business::setRating(double rating)
+{
+    this->rating = rating;
+}
+
+void Business::setMonth(int month)
+{
+    this->month = month;
+}
+
+void Business::setIncome(double income)
+{
+    this->income = income;
+}
+
+void Business::setExpenses(double expenses)
+{
+    this->expenses = expenses;
+}
+
+void Business::setWages(double wages)
+{
+    this->wages = wages;
+}
+
+void Business::setExecSalary(double execSalary)
+{
+    this->execSalary = execSalary;
+}
+
+void Business::setStores(int numStores)
+{
+    this->numStores = numStores;
+}
+
+void Business::setBuildings(int numBuildings)
+{
+    this->numBuildings = numBuildings;
+}
+
+void Business::setEmployees(int numEmployees)
+{
+    this->numEmployees = numEmployees;
+}
+
+std::string Business::getName()
+{
+    return name;
+}
+
+double Business::getCashOnHand()
+{
+    return cashOnHand;
+}
+
+double Business::getAssetsValue()
+{
+    return assetsValue;
+}
+
+double Business::getRating()
+{
+    return rating;
+}
+
+int Business::getMonth()
+{
+    return month;
+}
+
+double Business::getIncome()
+{
+    return income;
+}
+
+double Business::getExpenses()
+{
+    return expenses;
+}
+
+double Business::getWages()
+{
+    return wages;
+}
+
+double Business::getExecSalary()
+{
+    return execSalary;
+}
+
+int Business::getStores()
+{
+    return numStores;
+}
+
+int Business::getBuildings()
+{
+    return numBuildings;
+}
 #endif // BUSINESS
--- src/item.h
+++ src/item.h
@@ -15,67 +15,77 @@ class Item
         unsigned id;
 
     public:
-        Item(std::string name, int id, double producePrice, double buyPrice, double sellPrice)
-        {
-            this->name = name;
-            this->producePrice = producePrice;
-            this->buyPrice = buyPrice;
-            this->sellPrice = sellPrice;
-            this->id = id;
-            this->numOwned = 0;
-        }
+        Item(std::string name, int id, double producePrice, double buyPrice, double sellPrice);
+        void setNumOwned(unsigned numOwned);
+        void setProducePrice(double producePrice);
+        void setBuyPrice(double buyPrice);
+        void setSellPrice(double sellPrice);
+        std::string getName();
+        unsigned getID();
+        double getProducePrice();
+        double getBuyPrice();
+        double getSellPrice();
+        int getNumOwned();
 
-        void setNumOwned(unsigned numOwned)
-        {
-            this->numOwned = numOwned;
-        }
-
-        void setProducePrice(double producePrice)
-        {
-            this->producePrice = producePrice;
-        }
-
-        void setBuyPrice(double buyPrice)
-        {
-            this->buyPrice = buyPrice;
-        }
-
-        void setSellPrice(double sellPrice)
-        {
-            this->sellPrice = sellPrice;
-        }
-
-        std::string getName()
-        {
-            return name;
-        }
-
-        unsigned getID()
-        {
-            return id;
-        }
-
-        double getProducePrice()
-        {
-            return producePrice;
-        }
-
-        double getBuyPrice()
-        {
-            return buyPrice;
-        }
-
-        double getSellPrice()
-        {
-            return sellPrice;
-        }
-
-        int getNumOwned()
-        {
-            return numOwned;
-        }
 };
 
+Item::Item(std::string name, int id, double producePrice, double buyPrice, double sellPrice)
+{
+    this->name = name;
+    this->producePrice = producePrice;
+    this->buyPrice = buyPrice;
+    this->sellPrice = sellPrice;
+    this->id = id;
+    this->numOwned = 0;
+}
 
+void Item::setNumOwned(unsigned numOwned)
+{
+    this->numOwned = numOwned;
+}
 
+void Item::setProducePrice(double producePrice)
+{
+    this->producePrice = producePrice;
+}
+
+void Item::setBuyPrice(double buyPrice)
+{
+    this->buyPrice = buyPrice;
+}
+
+void Item::setSellPrice(double sellPrice)
+{
+    this->sellPrice = sellPrice;
+}
+
+std::string Item::getName()
+{
+    return name;
+}
+
+unsigned Item::getID()
+{
+    return id;
+}
+
+double Item::getProducePrice()
+{
+    return producePrice;
+}
+
+double Item::getBuyPrice()
+{
+    return buyPrice;
+}
+
+double Item::getSellPrice()
+{
+    return sellPrice;
+}
+
+int Item::getNumOwned()
+{
+    return numOwned;
+}
 #endif // ITEM
